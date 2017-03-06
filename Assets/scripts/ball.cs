80d2e3a90205b9dfc30e9a1c0ed8f2eed67aa96c
Jackie <grecoj2@hawkmail.newpaltz.edu>
2017-03-06 10:49:29 -0500

    Lives

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class ball : MonoBehaviour {
 
     public Vector2 startingVelocity = new Vector2(5, -20);
+    private Vector3 startingPosition; 
+    public GameObject gameOver;
+
+    int lives = 3;
 
     void Start () {
+        startingPosition = transform.position; 
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
    
 	}
@@ -15,11 +20,30 @@ public class ball : MonoBehaviour {
         if (transform.position.y < -3.5f) {
             GetOut();
       
-        } 
-	}
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
+    }
 
     void GetOut()
     {
         Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+
+        if (lives == 0)
+        {
+            DoGameOver();
+        }
+    }
+
+    void DoGameOver()
+    {
+        gameOver.SetActive(true);
     }
 }
