54c2639b762b1d870db5c0be2ca2b5a1c8f8e2f4
Jackie <grecoj2@hawkmail.newpaltz.edu>
2017-03-08 21:11:36 -0500

    Bricks need to get hit 3 times to break

--- Assets/scripts/Brick.cs
+++ Assets/scripts/Brick.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Brick : MonoBehaviour {
+    
+    int hits = 3;
+    // Use this for initialization
+    void Start ()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-
-
-
+    }
     void OnCollisionEnter2D(Collision2D coll)
     {
-        gameObject.SetActive(false);
+        hits = hits - 1;
+        if (hits == 0){
+            Break();
+        }
+    }
+
+    void Break(){
+    gameObject.SetActive (false);
+  
     }
 }
